7c26b568f374e72c3080fd7197af3415c276fd6f
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-03-12 20:09:05 +0000

    end(?)

--- Projeto/js/index.js
+++ Projeto/js/index.js
@@ -1,3 +1,24 @@
-$("document").ready(function(){
-    console.log("funciona");
+$(document).ready(function(){
+    $('html').animate({
+        scrollTop: 0
+     }, function(){
+        window.addEventListener('scroll', function (event) {
+            if (isInViewport($(".trigger-anim-open")[0])) {
+                $(".apresentacao").removeClass("start-size").addClass("teste");
+            }
+        }, false);
+     });
 });
+
+
+
+
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
