03fae1c093bc739141a761197ddcc2488b8546a8
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-03-12 22:15:57 +0000

    Comentado com sucesso

--- Projeto/js/index.js
+++ Projeto/js/index.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
+    //Função para returnar ao topo antes de aplicar o event listener ao scroll
     $('html').animate({
         scrollTop: 0
      }, function(){
+        //Função que liga o listener do evento scroll
         window.addEventListener('scroll', function (event) {
+            //verificar se está visivel
             if (isInViewport($(".trigger-anim-open")[0])) {
-                $(".apresentacao").removeClass("start-size").addClass("teste");
+                //retirar o start-size e adicionar a class de animação
+                $(".apresentacao").removeClass("start-size").addClass("anim");
             }
         }, false);
+        //TODO Melhorar esta função visto que só é chamada uma vez mas continua a aplicar a class "anim" 
      });
 });
 
 
 
-
+// Função para verificar se está visivel para o utilizador
+// https://vanillajstoolkit.com/helpers/isinviewport/
 var isInViewport = function (elem) {
 	var distance = elem.getBoundingClientRect();
 	return (
